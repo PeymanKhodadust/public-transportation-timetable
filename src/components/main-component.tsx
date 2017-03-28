7dcabe25e1bc245381fd6dc96568e4ed717d0127
PeymanKhodadust <peyman.khodadust@gmail.com>
2017-03-28 10:13:10 +0200

    added search-view

--- src/components/main-component.tsx
+++ src/components/main-component.tsx
@@ -1,18 +1,39 @@
 import * as React from "react";
-
 import { SearchView } from "./search-view";
+import {ConnectionCoordinates} from "./connection-coordinates";
 
 export interface MainProps {}
+export interface MainState {from: string, to: string, progress: state}
 
-export interface MainState {}
+const enum state{
+    search= 1,
+    result= 2,
+    detail= 3
+}
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the 'undefined' type.
 export class MainComponent extends React.Component<MainProps, MainState> {
+    constructor (props: any) {
+        super(props);
+        this.state = {
+            from: "",
+            to: "",
+            progress: state.search;
+        }
+    }
+    public handleOnChangeFrom (event: any) {
+        this.setState({from: event.target.value});
+        console.log("from: " + this.state.from + "\n");
+    }
+    public handleOnChangeTo (event: any) {
+        this.setState({to: event.target.value});
+        console.log("to: " + this.state.to + "\n");
+    }
     render() {
-        return (<div>
-          <h1>Main Component</h1>
-          <SearchView />
-        </div>);
+        return (
+            <div>
+              <h1>Main Component</h1>
+              <SearchView onChangeFrom={this.handleOnChangeFrom} onChangeTo={this.handleOnChangeTo} connectionCoordinates={ConnectionCoordinates}/>
+            </div>
+        );
     }
 }
--- src/components/search-view.tsx
+++ src/components/search-view.tsx
@@ -1,13 +1,18 @@
 import * as React from "react";
 
-export interface SearchViewProps {}
-
+export interface SearchViewProps {connectionCoordinates: any, onChangeFrom: any, onChangeTo: any}
 export interface SearchViewState {}
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the 'undefined' type.
 export class SearchView extends React.Component<SearchViewProps, SearchViewState> {
+    constructor(props: SearchViewProps) {
+        super(props);
+    }
+
     render() {
-        return <h1>Search View</h1>;
+        return (
+            <div>
+                <this.props.connectionCoordinates onChangeFrom={this.props.onChangeFrom} onChangeTo={this.props.onChangeTo}/>
+            </div>
+        );
     }
 }
